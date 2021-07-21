4b77e031628fe957e5a9ab762e880a48cba3d5dc
SylvesterVN <85309429+SylvesterVN@users.noreply.github.com>
2021-07-21 15:43:48 +0100

    Update and rename LocationTracker1.cs to LocationTracker.cs

--- LocationTracker.cs
+++ LocationTracker.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class LocationTracker1 : MonoBehaviour
+public class LocationTracker : MonoBehaviour
 {
     
     /*assign this script to a Game Manager and this sctipt will create a list of all the
      * available locations in the game by searching for all the objects with the LocationData sctipt.*/
 
 
-    public LocationData1[] location;
+    public LocationData[] location;
     
     void Start()
     {
-        location = GameObject.FindObjectsOfType<LocationData1>();
+        location = GameObject.FindObjectsOfType<LocationData>();
 
     }
 
